596c959d604c42b397655d6b98528159434ab94d
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-09-13 19:40:09 -0700

    complete

---
+++ src/Box.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Box(props) {
+  return (
+    <section class="box" id={props.id}>
+      <h3>{props.judul}</h3>
+      <p>
+        You can do <b>Anything</b>, but not everything
+      </p>
+      <ul>
+        <li>
+          You can do anything. <b>You can be anything</b> You can do things that
+          other people can.
+        </li>
+      </ul>
+      <p>
+        But the point is, <b>not everything</b> you have to be able to do, even
+        if you think "other people can, how can I?" Yes, those words are true.
+        But ... <b>please, you have to focus!</b>
+      </p>
+      <a href="#" target="_blank">
+        Read More
+      </a>
+    </section>
+  );
+}
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 export default function Header() {
   return (
     <header>
-      <h1 class="white">Classes & IDs</h1>
+      <h1 class="white">Nisa's Blog ❤</h1>
+      <p>The beautiful words I created</p>
+      <a href="https://nisacodelifings.netlify.com" target="_blank">
+        Website
+      </a>
+      <a href="https://facebook.com/nscdlg" target="_blank">
+        Facebook
+      </a>
+      <a href="https://wa.me/6289508320163" target="_blank">
+        Contact
+      </a>
     </header>
   );
 }
