d323da8120f6b2181fda70795ade1caf25b04d0f
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-09-13 17:35:20 -0700

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function Header() {
+  return (
+    <header>
+      <h1 class="white">Classes & IDs</h1>
+    </header>
+  );
+}
